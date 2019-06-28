55ab2a5b407f5b9186c3fdc2e1efae490a2424b7
Pedro Tavares <ptavaresh@gmail.com>
2019-06-28 11:01:37 -0500

    Learnign states

--- src/index.js
+++ src/index.js
@@ -4,12 +4,34 @@ import SeasonDisplay from './SeasonDisplay'
 
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        //THIS IS THE ONLY TIME THAT WE INITIALIZE STATE
+        this.state = { lat: null, errorMessage: '' };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            err => console.log(err)
+            position => {
+                //We called setState
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
             );
-        return <div>Latitude: </div>
+    }
+
+    //We have to define render
+    render() {        
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage }</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: { this.state.lat }</div>
+        }
+
+        return <div>Loading....</div>
+        
     }
 }
 
